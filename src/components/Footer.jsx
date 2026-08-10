bcd1b7b11541d56bc719d8ade3a6e9f90d43baf1
Lohith Lakkakula <lohithlakkakula06@gmail.com>
2026-08-10 17:45:25 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
             <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="https://lohith-lakkakula.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Lohith</a>
+              <a  href="https://www.linkedin.com/in/lohith-lakkakula" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Lohith</a>
             </p>
             <a className="text-dark fs-4" href="https://github.com/lohith-06" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
